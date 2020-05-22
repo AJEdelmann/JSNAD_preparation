66259a0fc7e03eda1b819e1302bbfbef9b1fd1f1
Albert Edelmann <albert.edelmann@gmail.com>
2020-05-22 16:47:19 +0200

    added time-server solution

---
+++ time-server.js
@@ -0,0 +1,18 @@
+const net = require('net');
+    
+zeroFill = (i) => (i < 10 ? '0' : '') + i;
+    
+now = () => {
+  const d = new Date()
+    return d.getFullYear() + '-' +
+            zeroFill(d.getMonth() + 1) + '-' +
+            zeroFill(d.getDate()) + ' ' +
+            zeroFill(d.getHours()) + ':' +
+            zeroFill(d.getMinutes())
+}
+    
+const server = net.createServer(socket => {
+    socket.end(now() + '\n')
+})
+    
+server.listen(Number(process.argv[2]));
